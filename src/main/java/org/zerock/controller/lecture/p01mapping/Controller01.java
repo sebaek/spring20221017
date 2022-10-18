f5c5c6906f99785a096ddb978e2a17e083f230e2
sebaek <sebaek@gmail.com>
2022-10-18 10:08:43 +0900

    requestmapping

--- src/main/java/org/zerock/controller/lecture/p01mapping/Controller01.java
+++ src/main/java/org/zerock/controller/lecture/p01mapping/Controller01.java
@@ -17,6 +17,17 @@ public class Controller01 {
 	public void method01() {
 		System.out.println("메소드 1번 일함");
 	}
+	
+	@RequestMapping("/ex01/sub02")
+	public void method02() {
+		System.out.println("메소드 2번 일함");
+	}
+	
+	// "/ex01/sub03" 요청시 일하는 메소드 작성
+	@RequestMapping("/ex01/sub03")
+	public void funtion03() {
+		System.out.println("메소드 3번 일함");
+	}
 }
 
 

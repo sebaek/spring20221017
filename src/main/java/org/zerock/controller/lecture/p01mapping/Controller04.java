21d0a8716259ef5c3047ff641ed62a31c6ad692a
sebaek <sebaek@gmail.com>
2022-10-18 10:50:28 +0900

    request method

--- src/main/java/org/zerock/controller/lecture/p01mapping/Controller04.java
+++ src/main/java/org/zerock/controller/lecture/p01mapping/Controller04.java
@@ -1,5 +1,31 @@
 package org.zerock.controller.lecture.p01mapping;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("ex04")
 public class Controller04 {
 
+	@RequestMapping("sub01")
+	public void method1() {
+		System.out.println("메소드01 일!!!");
+	}
+
+	@RequestMapping(value = "sub02", method = RequestMethod.GET)
+	public void method2() {
+		System.out.println("메소드02 일!!!");
+	}
+	
+	@RequestMapping(value = "sub02", method = RequestMethod.POST)
+	public void method3() {
+		System.out.println("메소드03 일!!!");
+	}
 }
+
+
+
+
+
+

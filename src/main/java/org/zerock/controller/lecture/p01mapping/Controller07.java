a420803bdfc4ebac02f53b418c18c69412fb43df
sebaek <sebaek@gmail.com>
2022-10-18 11:42:43 +0900

    params

--- src/main/java/org/zerock/controller/lecture/p01mapping/Controller07.java
+++ src/main/java/org/zerock/controller/lecture/p01mapping/Controller07.java
@@ -21,12 +21,29 @@ public class Controller07 {
 	
 	// get 방식
 	// sub02 경로
-	
+	@GetMapping("sub02")
+	public void method3() {
+		System.out.println("3번 메소드");
+	}
 	
 	
 	// get 방식
 	// sub02 경로
 	// address  request param
+	@GetMapping(path = "sub02", params = "address")
+	public void method4() {
+		System.out.println("4번 메소드");
+	}
+	
+	@GetMapping(path = "sub03", params = "address=seoul")
+	public void method5() {
+		System.out.println("5번 메소드");
+	}
+	
+	@GetMapping(path = "sub03")
+	public void method6() {
+		System.out.println("6번 메소드");
+	}
 }
 
 

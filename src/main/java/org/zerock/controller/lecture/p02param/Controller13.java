a20922160ba3bd13dce8b3db6322799fef308d31
sebaek <sebaek@gmail.com>
2022-10-18 16:24:52 +0900

    params

--- src/main/java/org/zerock/controller/lecture/p02param/Controller13.java
+++ src/main/java/org/zerock/controller/lecture/p02param/Controller13.java
@@ -3,6 +3,8 @@ package org.zerock.controller.lecture.p02param;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.zerock.domain.lecture.JavaBean01;
+import org.zerock.domain.lecture.JavaBean02;
+import org.zerock.domain.lecture.JavaBean03;
 
 @Controller
 @RequestMapping("ex13")
@@ -19,7 +21,15 @@ public class Controller13 {
 	}
 	
 	
+	@RequestMapping("sub02")
+	public void method2(JavaBean02 params) {
+		System.out.println(params);
+	}
 	
+	@RequestMapping("sub03")
+	public void method3(JavaBean03 params) {
+		System.out.println(params);
+	}
 	
 }
 
--- src/main/webapp/jsp/controller13.jsp
+++ src/main/webapp/jsp/controller13.jsp
@@ -26,6 +26,10 @@
 		이메일 : <input type="email" name="email"> <br>
 		<button>전송</button>
 	</form>
+	
+	<hr>
+	
+	<!-- form 만들기  -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 </body>
 </html>

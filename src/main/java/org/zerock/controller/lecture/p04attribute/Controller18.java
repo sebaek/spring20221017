f819c01e0532512300826a51e08b552994a3eee7
sebaek <sebaek@gmail.com>
2022-10-19 12:18:46 +0900

    model attribute

--- src/main/java/org/zerock/controller/lecture/p04attribute/Controller18.java
+++ src/main/java/org/zerock/controller/lecture/p04attribute/Controller18.java
@@ -3,17 +3,15 @@ package org.zerock.controller.lecture.p04attribute;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.zerock.domain.lecture.JavaBean01;
 
 @Controller
 @RequestMapping("ex18")
 public class Controller18 {
 
+	// /ex18/sub01?name=park&address=seoul&email=abc@daum.net
 	@RequestMapping("sub01")
-	public void method01(
-			@RequestParam("name")
-			@ModelAttribute("str")
-			String a) {
+	public void method01(@ModelAttribute("bean") JavaBean01 b) {
 		
 	}
 }

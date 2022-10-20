bcfa3a5de119d00da4d01dd0baca02afa88d0a56
sebaek <sebaek@gmail.com>
2022-10-20 11:13:25 +0900

    집합 함수

--- src/main/webapp/WEB-INF/sql/27aggregate.sql
+++ src/main/webapp/WEB-INF/sql/27aggregate.sql
@@ -8,8 +8,16 @@ FROM Products
 GROUP BY CategoryID;
 
 -- CategoryID별 가장 작은 값
+SELECT CategoryID, MIN(Price) FROM Products GROUP BY CategoryID;
 -- CategoryID별 평균 가격
+SELECT CategoryID, AVG(Price) FROM Products GROUP BY CategoryID;
 -- CategoryID별 가격 합
+SELECT CategoryID, SUM(Price) FROM Products GROUP BY CategoryID;
+
+
+-- 나라별 고객 수 (Customers)
+
+-- 도시별 공급자 수 (Suppliers)
 
 
 

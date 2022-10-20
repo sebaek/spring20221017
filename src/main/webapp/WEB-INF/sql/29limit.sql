102442c10147bd223eb670ae63a2537c62a133e7
sebaek <sebaek@gmail.com>
2022-10-20 12:14:09 +0900

    limit

--- src/main/webapp/WEB-INF/sql/29limit.sql
+++ src/main/webapp/WEB-INF/sql/29limit.sql
@@ -12,7 +12,32 @@ ORDER BY Price
 LIMIT 3;
 
 -- 가장 나이가 많은 직원 2명 조회
+SELECT * FROM Employees ORDER BY BirthDate LIMIT 2;
 -- 가장 나이가 어린 직원 2명 조회
+SELECT * FROM Employees ORDER BY BirthDate DESC LIMIT 2;
+
+-- LIMIT : 중간 record만 조회
+SELECT * 
+FROM Customers 
+WHERE CustomerID 
+ORDER BY CustomerID
+LIMIT 3, 3; -- 3번 인덱스부터 3개
+
+SELECT * 
+FROM Customers 
+WHERE CustomerID 
+ORDER BY CustomerID
+LIMIT 0, 3; -- 0번 인덱스부터 3개
+
+SELECT * 
+FROM Customers 
+WHERE CustomerID 
+ORDER BY CustomerID
+LIMIT 3; -- 위에서부터 3개
+
+-- 직원의 나이가 두번째,세번째 많은 사람 조회
+
+
 
 
 

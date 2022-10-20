2a013480675a43306f564a905626f84b26204da1
sebaek <sebaek@gmail.com>
2022-10-20 14:37:09 +0900

    page

--- src/main/java/org/zerock/controller/lecture/p06jdbc/Controller24.java
+++ src/main/java/org/zerock/controller/lecture/p06jdbc/Controller24.java
@@ -1,6 +1,7 @@
 package org.zerock.controller.lecture.p06jdbc;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.zerock.domain.lecture.Customer;
 
 @Controller
@@ -53,6 +55,50 @@ public class Controller24 {
 				list.add(c);
 			}
 		}
+	}
+
+	@GetMapping("sub02")
+	public String listCustomers(
+			@RequestParam(name = "page", defaultValue="1") int page,
+			@ModelAttribute("customerList") ArrayList<Customer> list) throws Exception {
+		
+		String sql = "SELECT "
+				+ "CustomerID id, "
+				+ "CustomerName name, "
+				+ "ContactName, "
+				+ "Address, "
+				+ "City, "
+				+ "PostalCode, "
+				+ "Country "
+				+ "FROM Customers "
+				+ "ORDER BY CustomerID DESC "
+				+ "LIMIT ?, ?";
+		
+		try (Connection con = dataSource.getConnection();
+				PreparedStatement pstmt = con.prepareStatement(sql)) {
+			pstmt.setInt(1, (10 * (page - 1)));
+			pstmt.setInt(2, 10);
+			
+			try (ResultSet rs = pstmt.executeQuery();) {
+				while (rs.next()) {
+					Customer c = new Customer();
+					c.setName(rs.getString("name"));
+					c.setAddress(rs.getString("address"));
+					c.setCity(rs.getString("city"));
+					c.setCountry(rs.getString("country"));
+					c.setPostalCode(rs.getString("postalCode"));
+					c.setId(rs.getInt("id"));
+					c.setContactName(rs.getString("contactName"));
+					
+					list.add(c);
+				}
+				
+			}
+			
+		}
+		
+		
+		return "ex24/sub01";
 	}
 }
 

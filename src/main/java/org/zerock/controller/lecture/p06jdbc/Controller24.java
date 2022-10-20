47ae47ae7001807167002046a08b73a9707ed3ae
sebaek <sebaek@gmail.com>
2022-10-20 14:25:55 +0900

    customer list

---
+++ src/main/java/org/zerock/controller/lecture/p06jdbc/Controller24.java
@@ -0,0 +1,68 @@
+package org.zerock.controller.lecture.p06jdbc;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.zerock.domain.lecture.Customer;
+
+@Controller
+@RequestMapping("ex24")
+public class Controller24 {
+	
+	@Autowired
+	private DataSource dataSource;
+
+	@GetMapping("sub01")
+	public void listAllCustomers(
+			@ModelAttribute("customerList") ArrayList<Customer> list) throws Exception {
+		
+		String sql = "SELECT "
+				+ "CustomerID id, "
+				+ "CustomerName name, "
+				+ "ContactName, "
+				+ "Address, "
+				+ "City, "
+				+ "PostalCode, "
+				+ "Country "
+				+ "FROM Customers "
+				+ "ORDER BY CustomerID DESC";
+		
+		try (Connection con = dataSource.getConnection();
+				Statement stmt = con.createStatement();
+				ResultSet rs = stmt.executeQuery(sql);) {
+			
+			while (rs.next()) {
+				Customer c = new Customer();
+				c.setName(rs.getString("name"));
+				c.setAddress(rs.getString("address"));
+				c.setCity(rs.getString("city"));
+				c.setCountry(rs.getString("country"));
+				c.setPostalCode(rs.getString("postalCode"));
+				c.setId(rs.getInt("id"));
+				c.setContactName(rs.getString("contactName"));
+				
+				list.add(c);
+			}
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+

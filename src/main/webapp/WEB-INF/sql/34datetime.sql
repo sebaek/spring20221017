83ed168132b5b5621f1952098dcd8217d61541ce
sebaek <sebaek@gmail.com>
2022-10-21 11:03:00 +0900

    date time

---
+++ src/main/webapp/WEB-INF/sql/34datetime.sql
@@ -0,0 +1,17 @@
+-- 날짜 시간
+
+-- DATE : 날짜
+-- DATETIME : 날짜 시간
+
+CREATE TABLE myTable06 (
+	col1 DATE, 
+    col2 DATETIME
+);
+INSERT INTO myTable06 (col1, col2) 
+VALUES ('2022-10-21', '2022-10-21 11:02:00');
+SELECT * FROM myTable06;
+
+
+
+
+

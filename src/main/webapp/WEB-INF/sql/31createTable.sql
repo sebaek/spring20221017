631e952358f8f7c3c2cf478739127495886d177a
sebaek <sebaek@gmail.com>
2022-10-21 09:33:01 +0900

    create database, create table

---
+++ src/main/webapp/WEB-INF/sql/31createTable.sql
@@ -0,0 +1,25 @@
+USE mydb1;
+
+-- 테이블 만들기
+CREATE TABLE myTable01 (
+	-- 컬럼명 자료형 나열
+    col1 VARCHAR(255),
+    col2 VARCHAR(255),
+    col3 VARCHAR(255)
+);
+
+SELECT * FROM myTable01;
+
+-- 테이블 구조 보기
+DESCRIBE myTable01;
+DESC myTable01;
+
+-- 새 테이블 만들기 
+-- 테이블명 myTable02
+-- 컬럼명 name, age, address
+-- 데이터 타입은 모두 varchar(255)
+
+
+
+
+

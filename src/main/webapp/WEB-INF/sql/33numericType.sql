14503625a208ae4e50c4eedbefea52ba82ffeef2
sebaek <sebaek@gmail.com>
2022-10-21 10:39:11 +0900

    numeric

--- src/main/webapp/WEB-INF/sql/33numericType.sql
+++ src/main/webapp/WEB-INF/sql/33numericType.sql
@@ -28,4 +28,24 @@ VALUES (22, 22, 22);
 SELECT * FROM myTable04;
 
 
+-- DEC : 소수점 있는 형식 수 저장
+CREATE TABLE myTable05 (
+	col1 DEC(3, 1), -- 총 길이 3, 소수점 아래 1
+    col2 DEC(5, 2) -- 총 길이 5, 소수점 아래 2
+);
+INSERT INTO myTable05 (col1, col2)
+VALUES (10.5, 200.01); -- ok
+INSERT INTO myTable05 (col1, col2)
+VALUES (100.5, 200.01); -- not ok
+INSERT INTO myTable05 (col1, col2)
+VALUES (10.55, 200.01); -- 반올림 입력됨
+SELECT * FROM myTable05;
+
+-- INT -> java int, long
+-- DEC -> java double, BigDecimal
+
+
+
+
+
 

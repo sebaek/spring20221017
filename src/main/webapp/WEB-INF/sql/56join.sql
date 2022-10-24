16f0d513f68f3c96ee3a838afbec20984593d5e6
sebaek <sebaek@gmail.com>
2022-10-24 16:16:00 +0900

    join

--- src/main/webapp/WEB-INF/sql/56join.sql
+++ src/main/webapp/WEB-INF/sql/56join.sql
@@ -11,6 +11,10 @@ FROM Products
 WHERE ProductID IN (11, 42, 72);
 
 -- '1996-07-04'에 주문된 상품명들 조회
+SELECT p.ProductName
+FROM Orders o JOIN OrderDetails d ON o.OrderID = d.OrderID
+              JOIN Products p ON d.ProductID = p.ProductID
+WHERE o.OrderDate = '1996-07-04';
 
 
 

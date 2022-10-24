acb0891b64b2820589e77cc5f6c2f24bf4057e8f
sebaek <sebaek@gmail.com>
2022-10-24 15:20:30 +0900

    join

--- src/main/webapp/WEB-INF/sql/54join.sql
+++ src/main/webapp/WEB-INF/sql/54join.sql
@@ -6,6 +6,13 @@ FROM Products p JOIN Suppliers s
      ON p.SupplierID = s.SupplierID;
 
 -- 'Tofu'상품을 공급하는 공급자가 사는 도시 조회
+SELECT p.ProductName, s.SupplierName, s.City
+FROM Products p JOIN Suppliers s
+  ON p.SupplierID = s.SupplierID
+WHERE p.ProductName = 'Tofu';
 -- 'Mayumi''s'가 공급하는 상품명들 조회
-
+SELECT p.ProductName
+FROM Products p JOIN Suppliers s
+  ON p.SupplierID = s.SupplierID
+WHERE s.SupplierName = 'Mayumi''s';
 

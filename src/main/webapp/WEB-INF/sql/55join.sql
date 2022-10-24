be1c2ae7439078edb7db57321168743718c02088
sebaek <sebaek@gmail.com>
2022-10-24 15:41:45 +0900

    join

---
+++ src/main/webapp/WEB-INF/sql/55join.sql
@@ -0,0 +1,37 @@
+SELECT * 
+FROM Customers c JOIN Orders o 
+                 JOIN Employees e;
+
+-- 총 rows : cRow * oRow * eRow
+-- 총 cols : cCol + oCol + eCol
+SELECT Count(*) FROM Customers; -- 91
+SELECT Count(*) FROM Employees; -- 9
+SELECT Count(*) FROM Orders; -- 830
+SELECT Count(*) 
+FROM Customers c JOIN Orders o 
+                 JOIN Employees e;
+                 
+DESC Customers; -- 7 cols
+DESC Orders; -- 5 cols
+DESC Employees; -- 6 cols
+
+
+SELECT c.CustomerName, e.FirstName, e.LastName
+FROM Customers c JOIN Orders o ON c.CustomerID = o.CustomerID
+                 JOIN Employees e ON o.EmployeeID = e.EmployeeID
+WHERE o.OrderDate = '1996-07-04';
+
+
+-- '1996-07-04'에 주문을 처리한 직원이름과 배송자명 조회
+
+
+
+
+
+                 
+                 
+                 ;
+                 
+                 
+                 
+                 

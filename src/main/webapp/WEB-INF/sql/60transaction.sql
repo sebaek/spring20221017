28ed206a04731e95b2758d6128724a62c92fc796
sebaek <sebaek@gmail.com>
2022-10-25 14:05:48 +0900

    transaction

---
+++ src/main/webapp/WEB-INF/sql/60transaction.sql
@@ -0,0 +1,14 @@
+-- UPDATE1
+-- UPDATE2
+USE w3schools;
+CREATE TABLE Bank(
+	customerId int,
+    balance int
+);
+
+INSERT INTO Bank (customerId, balance)
+VALUES (1, 10000)
+     , (2, 50000);
+     
+
+

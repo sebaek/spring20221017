6def300c853a25250405c71abe9b58bdf16ef702
sebaek <sebaek@gmail.com>
2022-10-25 10:10:01 +0900

    join

---
+++ src/main/webapp/WEB-INF/sql/57join.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE mydb6;
+USE mydb6;
+
+CREATE TABLE TableA (
+	col1 INT
+);
+
+CREATE TABLE TableB (
+	c1 INT
+);
+
+INSERT INTO TableA (col1)
+VALUES (1), (2), (3), (4), (5);
+INSERT INTO TableB (c1)
+VALUES (2), (4), (5), (7), (8);
+
+
+
+
+
+
+
+
+
+
+
+
+

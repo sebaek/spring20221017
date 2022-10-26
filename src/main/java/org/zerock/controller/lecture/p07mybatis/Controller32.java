d93171000e68cc3321d20adcd96069809ac0562d
sebaek <sebaek@gmail.com>
2022-10-26 15:06:47 +0900

    bean bean

--- src/main/java/org/zerock/controller/lecture/p07mybatis/Controller32.java
+++ src/main/java/org/zerock/controller/lecture/p07mybatis/Controller32.java
@@ -24,7 +24,44 @@ public class Controller32 {
 		JavaBean08 order = mapper.getOrder2();
 		System.out.println(order);
 	}
+	
+	@RequestMapping("sub03")
+	public void method3() {
+		/*
+SELECT 
+	p.productId,
+    p.productName,
+    p.price,
+    s.supplierId,
+    s.supplierName,
+    s.phone
+FROM 
+	Products p JOIN Suppliers s
+    ON p.SupplierID = s.SupplierID
+WHERE
+	p.ProductID = 10
+		 */
+		// '10', 'Ikura', '31.00', '4', 'Tokyo Traders', '(03) 3555-5011'
+
+		JavaBean09 product = mapper.getProduct();
+		System.out.println(product.getId()); // 10
+		System.out.println(product.getName()); // Ikura
+		System.out.println(product.getPrice()); // 31.00
+		System.out.println(product.getSupplier().getId()); // 4
+		System.out.println(product.getSupplier().getName()); // Tokyo Traders
+		System.out.println(product.getSupplier().getPhone()); // (03) 3555-5011
+		
+	}
 }
+
+
+
+
+
+
+
+
+
 
 
 

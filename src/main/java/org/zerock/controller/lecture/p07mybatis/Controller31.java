a01eb8e7af245ada73155a062f1f81e3b071498b
sebaek <sebaek@gmail.com>
2022-10-26 14:15:12 +0900

    product list

---
+++ src/main/java/org/zerock/controller/lecture/p07mybatis/Controller31.java
@@ -0,0 +1,33 @@
+package org.zerock.controller.lecture.p07mybatis;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.zerock.domain.lecture.JavaBean06;
+import org.zerock.mapper.lecture.Mapper04;
+
+@Controller
+@RequestMapping("ex31")
+public class Controller31 {
+	
+	@Autowired
+	private Mapper04 mapper;
+	
+	@RequestMapping("sub01")
+	public void method1() {
+		List<JavaBean06> productList = mapper.getProductList();
+		productList.forEach(System.out::println);
+	}
+}
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/org/zerock/mapper/lecture/Mapper04.java
@@ -0,0 +1,9 @@
+package org.zerock.mapper.lecture;
+
+import java.util.List;
+
+import org.zerock.domain.lecture.JavaBean06;
+
+public interface Mapper04 {
+	List<JavaBean06> getProductList();
+}

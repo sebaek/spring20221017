5128794d3925959df31dd73e38e211bfe8c4fb95
sebaek <sebaek@gmail.com>
2022-10-27 09:42:01 +0900

    getSupplier

--- src/main/java/org/zerock/controller/lecture/p07mybatis/Controller34.java
+++ src/main/java/org/zerock/controller/lecture/p07mybatis/Controller34.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.zerock.domain.lecture.JavaBean13;
+import org.zerock.domain.lecture.JavaBean14;
 import org.zerock.mapper.lecture.Mapper07;
+
+import com.sun.tools.javac.util.List;
 
 @Controller
 @RequestMapping("ex34")
@@ -23,6 +26,21 @@ public class Controller34 {
 			System.out.println(p.getName());
 			System.out.println(p.getPrice());
 		});
+	}
+	
+	@RequestMapping("sub02")
+	public void method2() {
+		JavaBean15 supplier = mapper.getSupplier();
+		System.out.println(supplier.getId());
+		System.out.println(supplier.getName());
+		
+		List<JavaBean14> product = supplier.getProduct();
+		product.forEach(p -> {
+			System.out.println(p.getId());
+			System.out.println(p.getName());
+			System.out.println(p.getPrice());
+		});
+		
 	}
 }
 

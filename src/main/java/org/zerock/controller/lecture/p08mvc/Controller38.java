d2badcaf523d838368f4b74f6e41ab2295e993a9
sebaek <sebaek@gmail.com>
2022-10-28 14:49:59 +0900

    service

--- src/main/java/org/zerock/controller/lecture/p08mvc/Controller38.java
+++ src/main/java/org/zerock/controller/lecture/p08mvc/Controller38.java
@@ -3,6 +3,7 @@ package org.zerock.controller.lecture.p08mvc;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.zerock.domain.lecture.JavaBean18;
 import org.zerock.service.lecture.Service01;
 
 @Controller
@@ -17,7 +18,9 @@ public class Controller38 {
 		// 리퀘스트파라미터 수집/가공
 		int id = 5;
 		// 비지니스 로직
-//		service.getCustomer(id);
+		JavaBean18 customer = service.getCustomer(id);
+		
+		System.out.println(customer);
 		// 모델에 어트리뷰트 추가
 		
 		// 포워드/리다이렉트
--- src/main/java/org/zerock/service/lecture/Service01.java
+++ src/main/java/org/zerock/service/lecture/Service01.java
@@ -1,8 +1,19 @@
 package org.zerock.service.lecture;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.zerock.domain.lecture.JavaBean18;
+import org.zerock.mapper.lecture.Mapper11;
 
 @Service
 public class Service01 {
+	
+	@Autowired
+	private Mapper11 mapper;
+
+	public JavaBean18 getCustomer(int id) {
+		// 비지니스 로직
+		return mapper.getCustomerById(id);
+	}
 
 }

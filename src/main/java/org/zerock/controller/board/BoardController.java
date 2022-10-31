2ed34c65e663b7dd5a4f34a69702c3411d120c80
sebaek <sebaek@gmail.com>
2022-10-31 09:45:58 +0900

    3 tier architecher

--- src/main/java/org/zerock/controller/board/BoardController.java
+++ src/main/java/org/zerock/controller/board/BoardController.java
@@ -1,14 +1,19 @@
 package org.zerock.controller.board;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.zerock.domain.board.BoardDto;
+import org.zerock.service.board.BoardSerivce;
 
 @Controller
 @RequestMapping("board")
 public class BoardController {
+	
+	@Autowired
+	private BoardSerivce service;
 
 	@GetMapping("register")
 	public void register() {
@@ -20,6 +25,9 @@ public class BoardController {
 	public void register(BoardDto board) {
 		// request param 수집/가공
 		System.out.println(board);
+		
+		// business logic
+		service.register(board);
 	}
 	
 	
---
+++ src/main/java/org/zerock/mapper/board/BoardMapper.java
@@ -0,0 +1,10 @@
+package org.zerock.mapper.board;
+
+import org.zerock.domain.board.BoardDto;
+
+public interface BoardMapper {
+
+	
+	int insert(BoardDto board);
+
+}

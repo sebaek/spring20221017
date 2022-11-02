9fc1f83e3dbb31107097acd6fc258edec7bc55f8
sebaek <sebaek@gmail.com>
2022-11-02 14:07:45 +0900

    send data

---
+++ src/main/java/org/zerock/controller/lecture/p09fetch/Controller43.java
@@ -0,0 +1,14 @@
+package org.zerock.controller.lecture.p09fetch;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("ex43")
+public class Controller43 {
+
+	@RequestMapping("sub")
+	public void method() {
+		
+	}
+}

791a82aa01b15021d958caa15899456388e57987
sebaek <sebaek@gmail.com>
2022-11-02 12:27:35 +0900

    accept header

---
+++ src/main/java/org/zerock/controller/lecture/p09fetch/Controller42.java
@@ -0,0 +1,28 @@
+package org.zerock.controller.lecture.p09fetch;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("ex42")
+public class Controller42 {
+
+	@RequestMapping("sub")
+	public void method0() {
+		
+	}
+	
+	@GetMapping(path = "sub01", produces = "text/html")
+	public void method1() {
+		System.out.println("/ex42/sub01 메소드 일함");
+	}
+}
+
+
+
+
+
+
+
+

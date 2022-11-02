b2508850b47cc1e8025dd73d3a9a6b8a7439d761
sebaek <sebaek@gmail.com>
2022-11-02 09:27:45 +0900

    fetch jsp

---
+++ src/main/java/org/zerock/controller/lecture/p09fetch/Controller39.java
@@ -0,0 +1,14 @@
+package org.zerock.controller.lecture.p09fetch;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("ex39")
+public class Controller39 {
+
+	@RequestMapping("sub01")
+	public void method1() {
+		
+	}
+}

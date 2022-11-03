8926f709883eb398fc5f95f11998cfe11e345add
sebaek <sebaek@gmail.com>
2022-11-03 15:18:07 +0900

    status  code

--- src/main/java/org/zerock/controller/lecture/p09fetch/Controller45.java
+++ src/main/java/org/zerock/controller/lecture/p09fetch/Controller45.java
@@ -1,6 +1,8 @@
 package org.zerock.controller.lecture.p09fetch;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -9,6 +11,25 @@ public class Controller45 {
 
 	@RequestMapping("sub")
 	public void method() {
-		
+
+	}
+
+	@GetMapping("sub01")
+	public ResponseEntity method01() {
+//		return ResponseEntity.ok().build();
+//		return ResponseEntity.badRequest().build();
+//		return ResponseEntity.notFound().build();
+		return ResponseEntity.status(500).build();
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
--- src/main/webapp/WEB-INF/views/ex45/sub.jsp
+++ src/main/webapp/WEB-INF/views/ex45/sub.jsp
@@ -12,6 +12,27 @@
 </head>
 <body>
 	<h1>응답!!</h1>
+	<button id="btn1">/ex45/sub01</button>
+	<br>
+	<button id="btn2">/ex45/sub02 202 accepted 응답</button>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+<script>
+const ctx = "${pageContext.request.contextPath}";
+
+document.querySelector("#btn1").addEventListener("click", function() {
+	fetch(ctx + "/ex45/sub01");
+});
+</script>
 </body>
 </html>
+
+
+
+
+
+
+
+
+
+
+

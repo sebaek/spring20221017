6f8ade24d8c52bee84c631f5c09ffa2a249565ea
sebaek <sebaek@gmail.com>
2022-11-04 11:02:11 +0900

    json respone

---
+++ src/main/java/org/zerock/controller/lecture/p09fetch/Controller47.java
@@ -0,0 +1,14 @@
+package org.zerock.controller.lecture.p09fetch;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("ex47")
+public class Controller47 {
+
+	@RequestMapping("sub")
+	public void method() {
+		
+	}
+}

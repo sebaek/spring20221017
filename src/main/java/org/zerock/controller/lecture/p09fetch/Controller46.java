e045406eaed4fd11b8761355227b4b3c157118c1
sebaek <sebaek@gmail.com>
2022-11-04 09:41:15 +0900

    fetch

--- src/main/java/org/zerock/controller/lecture/p09fetch/Controller46.java
+++ src/main/java/org/zerock/controller/lecture/p09fetch/Controller46.java
@@ -1,7 +1,9 @@
 package org.zerock.controller.lecture.p09fetch;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("ex46")
@@ -11,4 +13,18 @@ public class Controller46 {
 	public void method() {
 		
 	}
+	
+	@GetMapping("sub01")
+	@ResponseBody
+	public String method1() {
+		
+		return "hello world!!";
+	}
 }
+
+
+
+
+
+
+
--- src/main/webapp/WEB-INF/views/ex46/sub.jsp
+++ src/main/webapp/WEB-INF/views/ex46/sub.jsp
@@ -12,7 +12,14 @@
 </head>
 <body>
 	<h1>응답 처리</h1>
+	<button id="btn1">/ex46/sub01</button>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+<script>
+const ctx = "${pageContext.request.contextPath}";
+document.querySelector("#btn1").addEventListener("click", function() {
+	fetch(ctx + "/ex46/sub01");
+});
+</script>
 </body>
 </html>
 

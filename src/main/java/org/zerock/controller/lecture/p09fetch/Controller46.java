85a2bc66317b3e55e06c1bc0c608e9271b6ea6f2
sebaek <sebaek@gmail.com>
2022-11-04 09:37:35 +0900

    응답 처리

---
+++ src/main/java/org/zerock/controller/lecture/p09fetch/Controller46.java
@@ -0,0 +1,14 @@
+package org.zerock.controller.lecture.p09fetch;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("ex46")
+public class Controller46 {
+
+	@RequestMapping("sub")
+	public void method() {
+		
+	}
+}

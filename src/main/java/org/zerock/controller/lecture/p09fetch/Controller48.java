07dfb6ee9decb9bcfb0d11b90da26c661150ed6c
sebaek <sebaek@gmail.com>
2022-11-07 10:05:18 +0900

    list datas

---
+++ src/main/java/org/zerock/controller/lecture/p09fetch/Controller48.java
@@ -0,0 +1,27 @@
+package org.zerock.controller.lecture.p09fetch;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("ex48")
+public class Controller48 {
+
+	@GetMapping("sub")
+	public void method() {
+		
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
